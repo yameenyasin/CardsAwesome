c53d4a87bed16f52483389d309a20087bd78c2b4
Yameen Yasin <yameen@backbase.com>
2017-03-28 12:42:35 +0530

    Update widget config

--- app/widgetconfig.js
+++ app/widgetconfig.js
@@ -1,6 +1,9 @@
 require.config({
     paths: {
         "home-controller": "scripts/controllers/HomeController",
-        "card-awesome":"scripts/directives/cardAwesome"
+        "card-awesome":"scripts/directives/cardAwesome",
+        "config-service": "scripts/services/config",
+        "data-service": "scripts/services/dataService",
+        "route-resolver": "scripts/services/routeResolver"
     }
 });
